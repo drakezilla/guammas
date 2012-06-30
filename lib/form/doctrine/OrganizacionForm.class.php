b7bbfb9fc0eecd4e43bd1eb8dd7948eabf228c9d
drake <drake@localhost>
2012-06-29 19:45:36 -0430

    nuevo modulo: empresas 	Creada vista de nuevas empresas 		Creado partial de exito 		Creado formulario (pendiente de estilizar) 		Creados partial del mapa 	Modificada la clase de ciudad para mostrar el nombre y el nombre del estado 	Modificadas las clases categoria y estado para mostrar el nombre 	Agregado metodo en la clase organizacion que genera un token unico a las empresas 	Creados metodos javascripts personalizados 		Funcion para llamar al api de googlemaps 		Funcion para llamar al dialogo 		Agregadas mascaras en campos RIF y Telefono

--- lib/form/doctrine/OrganizacionForm.class.php
+++ lib/form/doctrine/OrganizacionForm.class.php
@@ -8,9 +8,22 @@
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class OrganizacionForm extends BaseOrganizacionForm
-{
-  public function configure()
-  {
-  }
+class OrganizacionForm extends BaseOrganizacionForm {
+
+    public function configure() {
+        unset(
+                $this['verificada'],
+                $this['usuario_id'],
+                $this['activa'],
+                $this['salt'],
+                $this['token'],
+                $this['created_at'],
+                $this['updated_at']
+             );
+        
+        $this->widgetSchema['categoria_id'] = new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Categoria'), 'add_empty' => ""), array("style" => "width: 205px", "data-placeholder" => "Seleccione una categoria"));
+        $this->widgetSchema["categoria_id"]->setLabel("Categoría");
+        $this->widgetSchema["nombre_organizacion"]->setLabel("Nombre o Razón Social");
+    }
+
 }
--- lib/form/doctrine/UbicacionForm.class.php
+++ lib/form/doctrine/UbicacionForm.class.php
@@ -8,9 +8,26 @@
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class UbicacionForm extends BaseUbicacionForm
-{
-  public function configure()
-  {
-  }
+class UbicacionForm extends BaseUbicacionForm {
+
+    public function configure() {
+        unset(
+                $this['principal'],
+                $this['detalle_direccion'],
+                $this['telefono_2'],
+                $this['verificada'],
+                $this['usuario_id'],
+                $this['created_at'],
+                $this['updated_at']
+             );
+        
+        
+        
+        $this->widgetSchema["ciudad_id"] = new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Ciudad'), 'add_empty' => ""), array("style" => "width: 205px", "data-placeholder" => "Seleccione una ciudad"));
+        $this->widgetSchema["telefono_1"]->setLabel("Algún Teléfono");
+        $this->widgetSchema["rif"]->setLabel("RIF");
+        $this->widgetSchema["coordenada_x"] = new sfWidgetFormInputHidden();
+        $this->widgetSchema["coordenada_y"] = new sfWidgetFormInputHidden();
+    }
+
 }

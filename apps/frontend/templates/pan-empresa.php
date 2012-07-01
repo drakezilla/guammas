fc46645eb894ca057c63e28554857f259be04ef1
drakezilla <dt.drake@gmail.com>
2012-06-30 22:58:41 -0430

    borrada linea de prueba

--- apps/frontend/templates/pan-empresa.php
+++ apps/frontend/templates/pan-empresa.php
@@ -6,7 +6,6 @@ if ($sf_user->hasCredential("Empresa")) {
 }
 ?>
 <div id="pan-empresa">
-    ahora si, vaya a dormir!!
     <p>Hola <strong><?php echo $sf_user->getAttribute("usuario_username", '', "user_vars") ?></strong>, ¿qué quieres hacer?</p>
     <?php if ($boolEmpresas): ?>
         <?php for ($i = 0; $i < count($empresas); $i++) : ?>

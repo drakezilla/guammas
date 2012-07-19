881d7452d0bfc344eeaf40600f629b7089708324
drake <drake@drake-pc>
2012-07-19 14:53:27 -0430

    Nueva caracteristica 	Formalizada la creacion de la Clase manejadora del api de googlemaps 		Modificados algunos metodos para hacerlos mas comunes a necesidades basicas 	Borrado borrador

--- web/js/custom/GoogleMapsApi.Class.js
+++ web/js/custom/GoogleMapsApi.Class.js
@@ -131,31 +131,16 @@ var GoogleMapClass = Class.extend({
             var localizacion= new google.maps.LatLng(jsonArray[k].coordenada_x,jsonArray[k].coordenada_y);
             eval('var marker'+k+'= new google.maps.Marker({position: localizacion,map: objMapa})');
             if(verIW){
-                if(jsonArray[k].principal==1){
-                    contenidoDialog = "<div><p><strong>Esta es tu sede principal</strong></p></div>"+
-                    "<div>Direccion: "+jsonArray[k].detalle_direccion+"</div>"+
-                    "<div>Telefono de contacto: "+jsonArray[k].telefono+"</div>"+
-                    "<div style='text-align: right'>"+
-                    "<img style='cursor: pointer' src='/images/stylistica-icons/24x24/edit.png' onclick='editarUbicacion("+jsonArray[k].id+")' />"+
-                    "<img style='cursor: pointer' src='/images/stylistica-icons/24x24/delete.png' onclick='eliminarUbicacion("+jsonArray[k].id+")' />"+
-                    "</div>"
-                }else{
-                    contenidoDialog = 
-                    "<div><p>Esta es una de tus sedes</p></div>"+
-                    "<div>Direccion: "+jsonArray[k].detalle_direccion+"</div>"+
-                    "<div>Telefono de contacto: "+jsonArray[k].telefono+"</div>"+
-                    "<div style='text-align: right'>"+
-                    "<img style='cursor: pointer' src='/images/stylistica-icons/24x24/edit.png' onclick='editarUbicacion("+jsonArray[k].id+")' />"+
-                    "<img style='cursor: pointer' src='/images/stylistica-icons/24x24/delete.png' onclick='confirmarDeleteUbicacion("+jsonArray[k].id+")' />"+
-                    "</div>"
-                }
+                contenidoDialog = "<div><p><strong>"+jsonArray[k].organizacion+"</strong></p></div>"+
+                "<div>Direccion: "+jsonArray[k].detalle_direccion+"</div>"+
+                "<div>Telefono de contacto: "+jsonArray[k].telefono+"</div>"+
+                "<div style='text-align: right'>"+
+                "</div>"
                 eval('var infowindow'+k+' = new google.maps.InfoWindow({content: contenidoDialog })');
                 eval('this.infoWindowArray.push(infowindow'+k+');')
                 eval('this.markersArray.push(marker'+k+');')
                 eval('google.maps.event.addListener(marker'+k+', "click", function() {infowindow'+k+'.open(objMapa,marker'+k+')})')
             }
-            
-            
         }
     },
     
@@ -215,105 +200,3 @@ var GoogleMapClass = Class.extend({
     }
     
 });
-
-var mapaNuevoEvento =  GoogleMapClass.extend({
-    mapActividad: function(){
-        var objMapa = this.objMapa
-        var markersArray = new Array()
-        var marker;
-        google.maps.event.addListener(objMapa, 'click', function(event){
-            for (i in markersArray) {
-                markersArray[i].setMap(null);
-            }
-            markersArray.length = 0;
-            marker = new google.maps.Marker({
-                position: event.latLng, 
-                animation: google.maps.Animation.DROP,
-                map: objMapa
-            });
-            markersArray.push(marker);
-            document.getElementById('anuncio_evento_coordenada_x').value = marker.position.lat();
-            document.getElementById('anuncio_evento_coordenada_y').value = marker.position.lng();
-        });
-    }
-})
-
-var mapaNuevaUbicacion = GoogleMapClass.extend({
-    mapActividad: function(){
-        
-        var objMapa = this.objMapa
-        var markersArray = new Array()
-        var infowindowArray = new Array()
-        this.actividad = google.maps.event.addListener(objMapa, 'click', function(event){
-            
-            for (i in markersArray) {
-                markersArray[i].setMap(null);
-            }
-            markersArray.length = 0;
-            var marker = new google.maps.Marker({
-                position: event.latLng, 
-                animation: google.maps.Animation.DROP,
-                map: objMapa
-            });
-            
-            var formNuevaUbicacion =
-            '<div>'+
-            '<input type="hidden" id="ubicacion_coordenada_x" value="'+marker.position.lat()+'" /><input type="hidden" id="ubicacion_coordenada_y" value="'+marker.position.lng()+'" />'+
-            '<table>'+
-            '<tr>'+
-            '<td>RIF</td>'+
-            '<td><input id="ubicacion_rif" onfocus="$(\'#ubicacion_rif\').mask(\'r-99999999-9\');" /></td>'+
-            '</tr>'+
-            '<tr>'+
-            '<td>Nombre</td>'+
-            '<td><input id="ubicacion_nombre" /></td>'+
-            '</tr>'+
-            '<tr>'+
-            '<td>Teléfono</td>'+
-            '<td><input id="ubicacion_telefono_ppal" onfocus="$(\'#ubicacion_telefono_ppal\').mask(\'9999-9999999\');" /></td>'+
-            '</tr>'+
-            '<tr>'+
-            '<td colspan="2" id="cell_btn_guardar"><button id="ubicacion_btn_guardar" type="button" onclick="guardarUbicacion()">Guardar</button></td>'+
-            '</tr>'+
-            '</table>'+
-            '</div>';    
-            
-            marker.infowindow = new google.maps.InfoWindow({
-                content: formNuevaUbicacion
-            })
-            
-            marker.infowindow.open(objMapa,marker)
-            google.maps.event.addListener(marker, "click", function() {
-                marker.infowindow.open(objMapa,marker)
-            })
-            markersArray.push(marker);
-            infowindowArray.push(marker.infowindow);
-        });
-        
-        this.markersFormArray=markersArray;
-        this.infoWindowArray=infowindowArray;
-    },
-    
-    cerrarIW: function(){
-        var marker = this.markersFormArray;
-        marker[0].infowindow.close();
-    },
-    
-    reinciarMapa: function(){
-        for (i in this.markersArray) {
-            this.markersArray[i].setMap(null);
-        }
-        
-        for (i in this.markersFormArray) {
-            this.markersFormArray[i].setMap(null);
-        }
-        
-        this.markersArray.length = 0;
-        
-        for (i in this.infoWindowArray) {
-            this.infoWindowArray[i].close();
-        }
-        this.infoWindowArray.length = 0;
-    }
-    
-})

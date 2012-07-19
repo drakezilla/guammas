a4679fa6b7ad4d7b7da11c1aad99bf62c35d28be
drake <drake@drake-pc>
2012-07-19 15:02:04 -0430

    Nueva caracteristica 	Reconstruida vista de nuevas empresas y organizaciones 		Corregida ruta de la libreria de google maps 		Eliminadas consultas ajax para traer el formulario 		Reescrito metodos carga del mapa a la nueva estructura de la libreria del api de googlemaps

--- apps/frontend/modules/empresa/templates/newSuccess.php
+++ apps/frontend/modules/empresa/templates/newSuccess.php
@@ -1,10 +1,2 @@
-<?php use_stylesheets_for_form($formOrganizacion) ?>
-<?php use_javascripts_for_form($formOrganizacion) ?>
-<?php use_javascript("jquery-plugins/jquery_maskedInput.js") ?>
-<?php use_javascript("jquery-plugins/jquery_chosen.js") ?>
-<?php use_stylesheet("jquery-plugins/jquery_chosen.css") ?>
-<?php use_stylesheet("modules/empresa/empresa_new.css") ?>
-<?php use_javascript("http://maps.google.com/maps/api/js?sensor=true&language=es") ?>
-<?php use_javascript('custom/googlemaps_api.js', 'last') ?>
-<?php include_partial("assets") ?>
+<?php include_partial("assets",array('formOrganizacion' => $formOrganizacion)) ?>
 <?php include_partial('form', array('formOrganizacion' => $formOrganizacion, 'formUbicacion' => $formUbicacion)) ?>

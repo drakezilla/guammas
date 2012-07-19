65d6a61dda61d00a824e79ea9739f476ac8afec0
drake <drake@drake-pc>
2012-07-19 15:00:02 -0430

    Nueva caracteristica 	Corregida ruta de la libreria de google maps 	Creado metodo para traer todas las ubicaciones del sistema

--- apps/frontend/modules/bienvenido/templates/_assets.php
+++ apps/frontend/modules/bienvenido/templates/_assets.php
@@ -1,5 +1,23 @@
+<?php use_javascript('http://maps.google.com/maps/api/js?sensor=true&language=es') ?>
+<?php use_javascript('custom/GoogleMapsApi.Class.js', 'last') ?>
 <script>
+    var gMap = new GoogleMapClass()
     $(document).ready(function(){
-        initializeNoInteraccion('map-container')
+        gMap.inicio(document.getElementById('map-container'),false)
+        getUbicaciones(false);
     })
+    
+    function getUbicaciones(recargar){
+        $.ajax({
+            url: '<?php echo url_for('@getUbicaciones') ?>',
+            dataType: 'json',
+            success: function(data){
+                ubicaciones = data
+                gMap.precargarUbicaciones(ubicaciones,true)
+                if(recargar){
+                    $("#popup-form").dialog('close')
+                }
+            }
+        })
+    }
 </script>
--- apps/frontend/modules/bienvenido/templates/indexSuccess.php
+++ apps/frontend/modules/bienvenido/templates/indexSuccess.php
@@ -1,5 +1,3 @@
-<?php use_javascript('http://maps.google.com/maps/api/js?sensor=true&language=es') ?>
-<?php use_javascript('custom/googlemaps_api.js', 'last') ?>
 <?php include_partial('assets') ?>
 <div class="span-24 last" id="map-container"></div>
 

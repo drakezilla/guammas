c3bb0b5f838592eb8a0d0fcef2bd212b618ef539
drake <drake@drake-pc>
2012-07-22 12:26:08 -0430

    Actualizacion 	borrado elemento map-container de los estilos 	Actualizado el diseño de la interfaz al nuevo estilo 		"convencionado" el modulo 		Los includes de estilos y javascripts especificos van en el asset

--- apps/frontend/modules/bienvenido/templates/_assets.php
+++ apps/frontend/modules/bienvenido/templates/_assets.php
@@ -1,12 +1,6 @@
 <?php use_javascript('http://maps.google.com/maps/api/js?sensor=true&language=es') ?>
 <?php use_javascript('custom/GoogleMapsApi.Class.js', 'last') ?>
 <script>
-    var gMap = new GoogleMapClass()
-    $(document).ready(function(){
-        gMap.inicio(document.getElementById('map-container'),false)
-        getUbicaciones(false);
-    })
-    
     function getUbicaciones(recargar){
         $.ajax({
             url: '<?php echo url_for('@getUbicaciones') ?>',
@@ -20,4 +14,33 @@
             }
         })
     }
+    
+    var gMap = new GoogleMapClass()
+    $(document).ready(function(){
+        gMap.inicio(document.getElementById('map_canvas'),false)
+        getUbicaciones(false);
+    })
+
+    $(document).ready(function(){
+        var hide=false;
+        $('#reg-empresa-image').click(function(){
+            if(!hide){
+                var ancho = '32px';
+                var margen= '318px';
+                hide = true;
+                $('#reg-empresa-image').html('<?php echo image_tag('stylistica-icons/24x24/left_arrow.png') ?>');
+                $('#reg-empresa-text').fadeOut('fast');
+            }else{
+                var ancho = '350px';
+                var margen= '0px';
+                hide = false;
+                $('#reg-empresa-image').html('<?php echo image_tag('stylistica-icons/24x24/right_arrow.png') ?>');
+                $('#reg-empresa-text').fadeIn('fast');
+            }
+            $(this).parent().animate({
+                width: ancho,
+                marginLeft: margen
+            })
+        });
+    })    
 </script>
--- apps/frontend/modules/bienvenido/templates/indexSuccess.php
+++ apps/frontend/modules/bienvenido/templates/indexSuccess.php
@@ -1,5 +1,2 @@
 <?php include_partial('assets') ?>
-<div class="span-24 last" id="map-container"></div>
-
-
-
+<div class="span-24 last" id="map_canvas" style="min-height: 1000px"></div>

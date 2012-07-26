b43fb3221cc7859b7647d228e61612f68f8ba565
drake <drake@drake-pc>
2012-07-25 21:12:39 -0430

    Actualizacion 	Ajustado el constructor de la clase para recibir el asunto del correo electronico

--- lib/swiftmailer.class.php
+++ lib/swiftmailer.class.php
@@ -4,17 +4,19 @@ class EmailClass {
 
     public $transport,
             $message,
-            $mailer;
+            $mailer,
+            $asunto;
 
-    public function __construct() {
+    public function __construct($asunto) {
         $this->transport = Swift_SmtpTransport::newInstance(sfConfig::get('app_correo_servidor_smtp'), sfConfig::get('app_correo_servidor_port'), sfConfig::get('app_correo_servidor_secu'))
                 ->setUsername(sfConfig::get('app_correo_usuario'))
                 ->setPassword(sfConfig::get('app_correo_contrasena'));
         $this->mailer = Swift_Mailer::newInstance($this->transport);
+        $this->asunto = $asunto;
     }
 
     public function correoExito($para, $cuerpo) {
-        $this->message = Swift_Message::newInstance('¡Cuenta creada!')
+        $this->message = Swift_Message::newInstance($this->asunto)
                 ->setContentType('text/html')
                 ->setFrom(array(sfConfig::get('app_correo_usuario') => sfConfig::get('app_correo_nombre_mostrar')))
                 ->setTo(array($para => 'Usuario'));

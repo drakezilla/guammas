c8953e91c2ced4bb2cb1da228593d414ce3ec29c
drake <drake@bunker-drake>
2012-07-26 18:57:07 -0430

    Nueva actualizacion 	Actualizado el view.yml de la aplicacion empresas 	Ajustado el layout de la aplicacion al nuevo diseño

--- apps/empresa/templates/layout.php
+++ apps/empresa/templates/layout.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
         <?php include_http_metas() ?>
         <?php include_metas() ?>
@@ -8,32 +8,27 @@
         <?php include_stylesheets() ?>
         <?php include_javascripts() ?>
     </head>
-    <body>
-        <div class="container" id="main-container">
-            <div class="span-4" id="lay-header-logo"></div>
-            <div class="span-20 last" id="lay-header-user-panel">
-                <div id="panel-empresa">
-                    <?php echo image_tag('stylistica-icons/24x24/search.png',array('id'=>'btn-pan-buscar')) ?>
-                    <?php echo image_tag('stylistica-icons/24x24/id_card.png',array('id'=>'btn-pan-empresa')) ?>
-                    <?php include 'pan-empresa.php'; ?>
-                    <?php //include 'pan-buscar.php'; ?>
-                </div>
-            </div>
-            <div class="span-2">
-                <div class="span-2 last" id="dash">
-                    <div class="span-2 last" id="top-dash"><a href="<?php echo url_for('@homepage') ?>"><?php echo image_tag('stylistica-icons/24x24/home.png') ?></a></div>
-                    <div class="span-2 last"><?php echo image_tag('stylistica-icons/24x24/user.png') ?></a></div>
-                    <div class="span-2 last"><a href="<?php echo url_for("@editarUsuario?nombre_usuario=".$sf_user->getAttribute('usuario_username','','user_vars'))?>"><?php echo image_tag('stylistica-icons/24x24/process.png') ?></a></div>
-                    <div class="span-2 last"><?php echo image_tag('stylistica-icons/24x24/comments.png') ?></div>
-                    <div class="span-2 last"><a href="<?php echo url_for('@logout')?>"><?php echo image_tag('stylistica-icons/24x24/shut_down.png') ?></a></div>
-                    <div class="span-2 last" id="end-dash"></div>
-                </div>
-                <div class="span-2 last" id="left-dash-footer"></div>
-            </div>
-            <div class="span-22 last" id="sf_content">
+    <body>        
+        <div class="container" id="main-wrapper">
+            <div id="main-content">
                 <?php echo $sf_content ?>
             </div>
+            <div class="span-4 last" id="lay-header-logo"></div>
+            <div id="lay-panel">
+                <?php echo image_tag('stylistica-icons/24x24/search.png', array('id' => 'btn-pan-buscar')) ?>
+                <?php echo image_tag('stylistica-icons/24x24/id_card.png', array('id' => 'btn-pan-empresa')) ?>
+                <?php include 'pan-empresa.php'; ?>
+            </div>
+            <div class="span-2 last" id="dash">
+                <div class="span-2 last"><a href="<?php echo url_for('@homepage') ?>"><?php echo image_tag('stylistica-icons/24x24/home.png') ?></a></div>
+                <div class="span-2 last"><?php echo image_tag('stylistica-icons/24x24/user.png') ?></div>
+                <div class="span-2 last"><a href="<?php echo url_for("@editarUsuario?nombre_usuario=" . $sf_user->getAttribute('usuario_username', '', 'user_vars')) ?>"><?php echo image_tag('stylistica-icons/24x24/process.png') ?></a></div>
+                <div class="span-2 last"><?php echo image_tag('stylistica-icons/24x24/comments.png') ?></div>
+                <div class="span-2 last"><a href="<?php echo url_for('@logout') ?>"><?php echo image_tag('stylistica-icons/24x24/shut_down.png') ?></a></div>
+                <div class="span-2 last" id="end-dash"></div>
+            </div>
+            <div id="lay-footer"></div>
         </div>
-        <div id="lay-footer"></div>
+        <div id="main-login-form"></div>
     </body>
 </html>
